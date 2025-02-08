e061d2d8f637d37dc298e219483c9cfcddf779b3
Mihu <mihu@123.com>
2025-02-08 22:39:08 +0800

    验证码库

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,10 +1,13 @@
 // Package helpers 存放辅助方法
 package helpers
 
-import ("reflect"
-        "time"
-        "fmt"
-        )
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"reflect"
+	"time"
+)
 
 // Empty 类似于 PHP 的 empty() 函数
 func Empty(val interface{}) bool {
@@ -35,4 +38,18 @@ func Empty(val interface{}) bool {
 // 输出为小数点后 3 位的 ms （microsecond 毫秒，千分之一秒）
 func MicrosecondsStr(elapsed time.Duration) string {
     return fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds())/1e6)
+}
+
+// RandomNumber 生成长度为 length 随机数字字符串
+func RandomNumber(length int) string {
+    table := [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+    b := make([]byte, length)
+    n, err := io.ReadAtLeast(rand.Reader, b, length)
+    if n != length {
+        panic(err)
+    }
+    for i := 0; i < len(b); i++ {
+        b[i] = table[int(b[i])%len(table)]
+    }
+    return string(b)
 }
